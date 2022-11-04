cdd56562348c549d51c5a2487257a5da4c6f6e32
TahaAhmedd <teto67523@gmail.com>
2022-11-04 19:13:51 +0200

    Add jop Screen

---
+++ screens/AddJop.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddJop = () => {
+  return (
+    <View>
+      <Text>AddJop</Text>
+    </View>
+  )
+}
+
+export default AddJop
+
+const styles = StyleSheet.create({})
