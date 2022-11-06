bd2ce96bc105cd11daa3896570031a462e99861c
mostafahpater <[mostafahpater@gamil.com>
2022-11-06 18:34:58 +0200

    add AllSanai3y Screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 import { Provider } from "react-redux";
-import { Store } from "./Redux/store";
+import { Store } from "./Redux/Store";
 import Index from "./screens/index";
 
 export default function App() {
