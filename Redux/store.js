b63d05e6a33819d9c50c2e1f9232511154471124
Mohamed2627 <[C[C[Dmohamed.ragab.elsaid262@gmail.com>
2022-11-07 14:45:39 +0200

    Store

--- Redux/store.js
+++ Redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import ClientReducer from "./Slices/ClientReducer"
-import currentRecieverReducer from "./Chat/ImageUrlSlicer"
 import Snai3yReducer from "./Slices/Snai3yReducer"
 export const Store = configureStore({
     reducer:
     {
         Snai3yReducer,
-        ClientReducer,
-        currentRecieverReducer
+        ClientReducer
     },
 
 })
